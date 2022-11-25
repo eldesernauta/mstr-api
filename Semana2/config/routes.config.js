a45669851b41a6022ff8d65f75ba212c690f116a
oscarspot <oscar@getspot.com>
2022-11-25 12:00:33 -0500

    Semana2

---
+++ Semana2/config/routes.config.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const posts = require('../controllers/posts.controller')
+
+// Routes here
+router.get('/posts', posts.list)
+router.post('/posts', posts.create);
+router.get('/posts/:ixd', posts.detail);
+router.patch('/posts/:id', posts.edit);
+router.delete('/posts/:id', posts.delete);
+
+module.exports = router;
